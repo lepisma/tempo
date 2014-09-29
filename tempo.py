204f957c39ce6b3374107de2eccd1908f8c752fd
lepisma <abhinav.tushar.vs@gmail.com>
2014-09-29 11:30:48 +0530

    Add dash

--- tempo.py
+++ tempo.py
@@ -1,6 +1,6 @@
 # Main script
 
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore, QtWebKit
 
 class RightClickMenu(QtGui.QMenu):
     """
@@ -9,6 +9,11 @@ class RightClickMenu(QtGui.QMenu):
 
     def __init__(self, parent):
         QtGui.QMenu.__init__(self, "tempo")
+        
+        # Show dash
+        dash_action = QtGui.QAction("Dashboard", self)
+        dash_action.triggered.connect(parent.show_dash)
+        self.addAction(dash_action)
 
         # Exit tempo
         exit_action = QtGui.QAction("Exit tempo", self)
@@ -25,11 +30,60 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
         self.setIcon(QtGui.QIcon("./icons/1.png"))
         self.setContextMenu(RightClickMenu(self))
 
+        self.activated.connect(self.click_trap)
+
+    def click_trap(self, value):
+        if value == self.Trigger:
+            self.show_dash()
+
+    def welcome(self):
+        self.showMessage("tempo", "Running in tray")
+
+    def show_dash(self):
+        if self.parent().isMinimized():
+            self.parent().setWindowState(QtCore.Qt.WindowActive)
+        self.parent().show()
+
+    def show(self):
+        QtGui.QSystemTrayIcon.show(self)
+        QtCore.QTimer.singleShot(100, self.welcome)
+
+class MainWindow(QtGui.QWidget):
+    """
+    Main window
+    """
+
+    def __init__(self, parent = None):
+        QtGui.QWidget.__init__(self, parent)
+
+        self.tray_icon = SystemTrayIcon(self)
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.resize(200, 400)
+        self.setWindowTitle("tempo")
+        self.setWindowIcon(QtGui.QIcon("./icons/1.png"))
+        self.layout = QtGui.QGridLayout(self)
+        self.layout.setMargin(0)
+        self.layout.setSpacing(0)
+
+        self.view = QtWebKit.QWebView(self)
+        view_url = "./view/dash.html"
+        self.view.setUrl(QtCore.QUrl(view_url))
+
+        self.layout.addWidget(self.view, 0, 0, 1, 1)
+        self.show()
+
+    def closeEvent(self, event):
+        self.hide()
+        event.ignore()
+
 def main():
     # Initialize Qt Application
     app = QtGui.QApplication([])
 
-    parent_widget = QtGui.QWidget()
+    parent_widget = MainWindow()
     tray_icon = SystemTrayIcon(parent_widget)
     tray_icon.show()
     app.exec_()
