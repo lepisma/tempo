5dfc684b4f27b0b0dbe342c8fb4afe52a3901bf6
lepisma <abhinav.tushar.vs@gmail.com>
2014-10-02 15:17:55 +0530

    Add sending in sketch

--- sketch/tempo/tempo.ino
+++ sketch/tempo/tempo.ino
@@ -1,3 +1,9 @@
+/*
+Arduino sketch for tempo.
+When gets 'A' from python code, it reads the value from LM35
+and returns the temperature after sending 'ok\n'
+*/
+
 float temp;
 int reading;
 
@@ -5,10 +11,20 @@ void setup()
 {
   analogReference(INTERNAL);
   int temp_pin = 0;
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  reading = analogRead(temp_pin);
-  temp = reading / 9.31;
+  if (Serial.available() > 0){
+    incoming = Serial.read();
+    if (incoming == 'A'){
+      // Reading
+      reading = analogRead(temp_pin);
+      temp = reading / 9.31;
+      // Sending
+      Serial.println("ok");
+      Serial.println(temp);
+    }
+  }
 }
